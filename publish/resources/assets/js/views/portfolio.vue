8280098ff961f955decd73aeecd655795a6b6d59
Unknown <cerny.vil@gmail.com>
2018-03-07 17:26:16 +0100

    + expand all

--- publish/resources/assets/js/views/portfolio.vue
+++ publish/resources/assets/js/views/portfolio.vue
@@ -1,21 +1,34 @@
 <template>
 	<div id="portfolio">
-		<div v-for="(model, index) in models" class="portfolio_item hero" :id="'hero-'+model.slug"  :model="model">
+
+		<h1 class="title is-1 has-text-centered">
+			<router-link to="/portfolio/all" @click.native="showAll()">ALL</router-link>
+		</h1>
+
+		<div v-for="(portfolio, index) in portfolios" class="portfolio_item hero" :id="'hero-'+portfolio.slug"  :portfolio="portfolio">
 			<div class="hero-body border-OFFX-r">
 				<div class="container border-OFFX-g">
 
-					<router-link :to="{ path: '/portfolio/' + model.slug }" @click.native="fetchChildData(model.slug)">
-						<div class="portfolio-background" :style="{ backgroundImage: 'url(' +  model.image_url + ')' }">
-							<h2 class="title is-1 ">{{ model.title }}</h2>
+					<router-link
+						:to="{ path: '/portfolio/' + portfolio.slug }"
+						:class="{ shown: isShown(portfolio.slug) }"
+						@click.native="toggle(portfolio.slug)"
+					>
+						<div class="portfolio-background" :style="{ backgroundImage: 'url(' +  portfolio.image_url + ')' }">
+							<h2 class="title is-1 ">{{ portfolio.title }}</h2>
 						</div>
 					</router-link>
-
 					<div class="hero-foot portfolio-detail border-OFF-r">
 						<transition name="show">
-							<portfolio-item v-if="show==model.slug || show=='all'" :portfolio_slug="model.slug"></portfolio-item>
+							<portfolio-item
+								:portfolio_slug="portfolio.slug"
+								v-if="isFetched(portfolio.slug)"
+								v-show="isShown(portfolio.slug)"
+							></portfolio-item>
 						</transition>
 
 					</div>
+
 
 				</div>
 			</div>
@@ -27,7 +40,6 @@
 
 </template>
 
-
 <style>
 
 	.show-enter,
@@ -37,7 +49,7 @@
 	.show-enter-to { max-height: 4096px; opacity: 1  }
 
 	.show-enter-active,
-	.show-leave-active { transition: 600ms ease-in}
+	.show-leave-active { transition: 600ms ease-out}
 
 	/*.show-enter-active { transition:  500ms }*/
 	/*.show-leave-active { transition:  1500ms }*/
@@ -52,37 +64,86 @@
 
 		data(){
 			return {
-				show:	null,
-				//shown:	null,
-				models:	[],
+				portfolios:	[],
 				fetched:	[],
+				shown:	[],
 			};
 		},
 		created(){
-			this.fetchChildData( this.$route.params.portfolio_slug );
-			axios.get('/api/get-portfolio').then( response => this.models = response.data );
-		},
-		mounted(){
-
+			//this.show( this.$route.params.portfolio_slug );
+			axios.get('/api/get-portfolio').then( response => this.portfolios = response.data );
 		},
 		methods:{
 
-			fetchChildData(portfolio_slug){
-				console.log('fetchChildData: '+portfolio_slug);
+			fetchPorfolioItem(portfolio_slug)
+			{
+				if( ! this.isFetched(portfolio_slug))
+					this.fetched.push(portfolio_slug);
+			},
 
-				this.show = portfolio_slug;
-				//this.show = 'all';
-				//if(this.fetched.indexOf(portfolio_slug)== -1)
-				//	this.fetched.push(portfolio_slug);
+			show(portfolio_slug)
+			{
+				this.fetchPorfolioItem(portfolio_slug);
+				if( ! this.isShown(portfolio_slug))
+					this.shown.push(portfolio_slug);
+			},
 
+			hide(portfolio_slug)
+			{
+				this.shown.splice(this.shown.indexOf(portfolio_slug), 1);
+			},
+			showAll()
+			{
+				for(var p=0; p<this.portfolios.length;p++)
+					this.show(this.portfolios[p].slug);
+			},
+
+			toggle(portfolio_slug)
+			{
+				this.fetchPorfolioItem(portfolio_slug);
+
+				if( ! this.isShown(portfolio_slug)){
+					this.show(portfolio_slug);
+					this.scrollToItem(portfolio_slug);
+				} else
+					this.hide(portfolio_slug);
+			},
+
+			isFetched(portfolio_slug)
+			{
+				return this.fetched.indexOf(portfolio_slug) > -1;
+			},
+
+			isShown(portfolio_slug)
+			{
+				return this.shown.indexOf(portfolio_slug) > -1;
+			},
+
+			scrollToItem(portfolio_slug){
 				setTimeout( function(){
 					VueScrollTo.scrollTo( "#hero-"+portfolio_slug, 500, { easing: 'ease-in', offset: -20});
-					//VueScrollTo.scrollTo( "#hero-"+portfolio_slug, 2000, {easing: 'ease-in'});
 				}, 1000);
 			}
+
 		},
+
 		components:{
 			'portfolio-item':portfolioItem,
 		}
 	};
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
